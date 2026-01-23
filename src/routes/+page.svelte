8007a6a9bd53c8a5880cae95230de1069ca99c50
Emmanuel <princeabelle5@gmail.com>
2026-01-23 11:45:09 +0300

    fix email contact

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -57,7 +57,7 @@
             },
             {
                 name: "Email",
-                url: "mailto:emmanuel.abelle@gmail.com",
+                url: "mailto:abelle.emmanuel@gmail.com",
                 icon: "email",
             },
         ]}
